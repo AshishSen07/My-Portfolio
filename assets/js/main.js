1a663c0403c6587377bd0544ebc6602b1e4de24d
Ashish Sen <ashishsen0706@gmail.com>
2025-04-21 12:05:54 +0530

    changes in cursor and hamburgur

--- assets/js/main.js
+++ assets/js/main.js
@@ -1324,63 +1324,46 @@ const coords = { x: 0, y: 0 };
 const circles = document.querySelectorAll(".circle");
 
 const colors = [
-  "#E9E0FD",
-  "#D8C8FB",
-  "#C7B1FA",
-  "#B799F8",
-  "#A682F6",
-  "#9570F1",
-  "#8B60ED",
-  "#814CEC",
-  "#7444D4",
-  "#683EBD",
-  "#5B38A5",
-  "#4F328D",
-  "#442C76",
-  "#38275E",
-  "#2D2147",
-  "#231B31",
-  "#1A1624",
-  "#130F1A",
-  "#0D0912",
-  "#08050C",
-  "#050308",
-  "#020103"
-]
+  "#E9E0FD", "#D8C8FB", "#C7B1FA", "#B799F8", "#A682F6", "#9570F1",
+  "#8B60ED", "#814CEC", "#7444D4", "#683EBD", "#5B38A5", "#4F328D",
+  "#442C76", "#38275E", "#2D2147", "#231B31", "#1A1624", "#130F1A",
+  "#0D0912", "#08050C", "#050308", "#020103"
+];
 
+// Check if it's a desktop device
+const isDesktop = !('ontouchstart' in window) && window.innerWidth > 768;
 
-circles.forEach(function (circle, index) {
-  circle.x = 0;
-  circle.y = 0;
-  circle.style.backgroundColor = colors[index % colors.length];
-});
-
-window.addEventListener("mousemove", function (e) {
-  coords.x = e.clientX;
-  coords.y = e.clientY;
-
-});
-
-function animateCircles() {
-
-  let x = coords.x;
-  let y = coords.y;
-
+if (isDesktop) {
   circles.forEach(function (circle, index) {
-	circle.style.left = x - 12 + "px";
-	circle.style.top = y - 12 + "px";
-
-	circle.style.scale = (circles.length - index) / circles.length;
-
-	circle.x = x;
-	circle.y = y;
-
-	const nextCircle = circles[index + 1] || circles[0];
-	x += (nextCircle.x - x) * 0.3;
-	y += (nextCircle.y - y) * 0.3;
+    circle.x = 0;
+    circle.y = 0;
+    circle.style.backgroundColor = colors[index % colors.length];
   });
 
-  requestAnimationFrame(animateCircles);
-}
+  window.addEventListener("mousemove", function (e) {
+    coords.x = e.clientX;
+    coords.y = e.clientY;
+  });
 
-animateCircles();
+  function animateCircles() {
+    let x = coords.x;
+    let y = coords.y;
+
+    circles.forEach(function (circle, index) {
+      circle.style.left = x - 12 + "px";
+      circle.style.top = y - 12 + "px";
+      circle.style.scale = (circles.length - index) / circles.length;
+
+      circle.x = x;
+      circle.y = y;
+
+      const nextCircle = circles[index + 1] || circles[0];
+      x += (nextCircle.x - x) * 0.3;
+      y += (nextCircle.y - y) * 0.3;
+    });
+
+    requestAnimationFrame(animateCircles);
+  }
+
+  animateCircles();
+}
